1600e12e865d444c01dd3b95be46c33c647a2239
fatosbeka <fatos_beka@hotmail.com>
2025-02-03 01:33:18 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,20 +1,15 @@
 import streamlit as st
-import requests  # Import requests to call the backend API
+import requests
 
-# Define the backend API URL (Replace with your Railway backend URL)
 BACKEND_URL = "https://romantic-sparkle-production-5f52.up.railway.app"
 
 st.title("Welcome to ITA AI")
 st.write("Ask your questions below:")
 
 user_input = st.text_input("Your question:")
-
 if user_input:
-    # Send the user input to the backend API
     response = requests.get(f"{BACKEND_URL}", params={"question": user_input})
-    
     if response.status_code == 200:
-        # Display the response from backend
         st.write(f"Backend Response: {response.text}")
     else:
         st.write("Error: Could not fetch response from backend")
